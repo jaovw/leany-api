bd7dca4babecc72ef46c98eb2f8cfeb3538cdf44
joaowonccepro <joao.woncce@prodigitalhr.com.br>
2025-06-23 15:15:58 -0300

    leany-api: - add example swagger controller decorators

--- src/trainer/trainer.controller.ts
+++ src/trainer/trainer.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Get, Post, Body, Param, Delete, Put } from '@nestjs/common'
 import { TrainerService } from './trainer.service';
 import { CreateTrainerDto } from './dto/create-trainer.dto';
 import { UpdateTrainerDto } from './dto/update-trainer.dto';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Trainers')
 @Controller('trainers')
 export class TrainerController {
   constructor(private readonly trainerService: TrainerService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Criar treinador' })
+  @ApiBody({ type: CreateTrainerDto })
+  @ApiResponse({ status: 201, description: 'Treinador criado com sucesso' })
   create(@Body() dto: CreateTrainerDto) {
     return this.trainerService.create(dto);
   }
